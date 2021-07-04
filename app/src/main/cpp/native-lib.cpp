0966315192e5debe4ea26d43a421d448eae69073
knclj <clj1017772778@gmail.com>
2021-07-04 21:40:37 +0800

    部署rtmp和x264 lib库

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,15 @@
 #include <jni.h>
 #include <string>
+//extern "C" rtmp 内部库已兼容
+#include <rtmp.h>
+#include <x264.h>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_test_rtmpdemo_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    char version[50];
+    sprintf(version,"librtmp version:%d",RTMP_LibVersion());
+    x264_picture_t * picture = new x264_picture_t ;
+    return env->NewStringUTF(version);
 }
